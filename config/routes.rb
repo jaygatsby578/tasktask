b12db8b06c7f8b9c5622f4126feb24a6225763ad
jaygatsby578 <jaygatsby1996@icloud.com>
2020-08-12 14:41:20 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+    root 'tasks#index'
+
+    get 'signup', to: 'users#new'
+    post 'signup', to: 'users#create'
+
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
+    resources :users, only: [] do
+        resources :tasks, only: :index
+    end
+    resources :tasks, only: %i[create update]
+    
+   namespace :api do
+     get 'login', to: 'login#show'
+     resources :tasks, only: %i[index create update]
+   end
+end
